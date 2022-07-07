7ee54bbb5f2635a068e1af4b4207598bf49ce5d5
John Lambert <johnla@microsoft.com>
2022-07-07 15:38:15 -0700

    Address handle leaks in `GetSystem`
    `GetSystem` leaks token handles in error code paths.

--- Koh/Helpers.cs
+++ Koh/Helpers.cs
@@ -176,6 +176,7 @@ namespace Koh
                 success = Interop.DuplicateToken(hToken, 2, ref hDupToken);
                 if (!success)
                 {
+                    CloseHandle(hToken);
                     Console.WriteLine("  [!] GetSystem() - DuplicateToken failed!");
                     return false;
                 }
@@ -183,6 +184,8 @@ namespace Koh
                 success = Interop.ImpersonateLoggedOnUser(hDupToken);
                 if (!success)
                 {
+                    CloseHandle(hToken);
+                    CloseHandle(hDupToken);
                     Console.WriteLine("  [!] GetSystem() - ImpersonateLoggedOnUser failed!");
                     return false;
                 }
